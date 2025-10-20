330bfb25916515241b80840881452f35ea84bf75
daniel.li <daniel.li@markrtingforce.com>
2025-10-20 20:21:04 +0800

    muc init

---
+++ src/main/java/com/bdsw/mucserver/MucServerApplication.java
@@ -0,0 +1,13 @@
+package com.bdsw.mucserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MucServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MucServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bdsw/mucserver/MucServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdsw.mucserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MucServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
